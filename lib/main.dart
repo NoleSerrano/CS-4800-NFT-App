2c7fd0f4985bddee380750722608684ad4c51d88
Nole Serrano <43283288+NoleSerrano@users.noreply.github.com>
2022-03-25 15:39:42 -0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:android_deso_app/Screens/Components/my_input_theme.dart';
+import 'package:android_deso_app/Screens/HomePage/home_page2.dart';
+import 'package:android_deso_app/Screens/HomePage/settings_page.dart';
+import 'package:android_deso_app/Screens/Settings/update_wallet_page.dart';
+import 'package:flutter/material.dart';
+import 'Screens/HomePage/home_page.dart';
+
+void main() {
+  runApp(const DeSoApp());
+}
+
+class DeSoApp extends StatelessWidget {
+  const DeSoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeSo App',
+      theme: ThemeData(
+        inputDecorationTheme: MyInputTheme().theme(),
+      ),
+      home: const HomePage2(),
+
+    );
+  }
+}
